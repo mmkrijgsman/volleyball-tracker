3b88fcc3760b2ef838651b425051af38ecdc1cfc
mmkrijgsman <m3krijgsman@gmail.com>
2025-12-14 00:43:43 +0100

    Update VolleyballTracker.jsx

--- src/VolleyballTracker.jsx
+++ src/VolleyballTracker.jsx
@@ -1,1 +1,10 @@
-chore: clear broken VolleyballTracker
+import React from 'react';
+
+export default function VolleyballTracker() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>🏐 Volleyball Match Tracker</h1>
+      <p>Deploy test – als je dit ziet is alles goed</p>
+    </div>
+  );
+}
